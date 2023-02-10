d92d5fb45637e307331aa67720d6172d78a8ecfc
HarshaMacherla <macherlaharsha997@gmail.com>
2023-02-10 11:17:07 +0530

    Now multiple user details can be added

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var forSubmit = document.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   let userName = document.getElementById('name').value;
   let userEmail = document.getElementById('email').value;
+  let userMobile = document.getElementById('mobile').value;
 
   let userDetails = {
     "User Name": userName,
-    "Email ID": userEmail
+    "Mobile Number": userMobile
   };
 
-  localStorage.setItem("User Details", JSON.stringify(userDetails));
+  localStorage.setItem(userEmail, JSON.stringify(userDetails));
 }
